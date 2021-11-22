82e712473ae8b00f2d246cde9bbad3bb6b0c1333
Nik980 <battlefield980@mail.ru>
2021-11-22 20:36:11 +0500

    add new features

--- stupnikov.py
+++ stupnikov.py
@@ -4,8 +4,53 @@ print("Доступные комманды:")
 print("1 - добавить новое наименование")
 print("2 - просмотреть список товаров")
 print("3 - выйти")
+print("4 - Считать из файла")
+print("5 - Статистика")
+print("6 - удалить позицию")
 
 items = []
+
+
+def readfile():
+    f = open("filename.txt", "r")
+    lines = f.readlines()
+
+    for line in lines:
+        splitline = line.split(" ")
+        item = {
+            "name": splitline[0],
+            "count": splitline[1],
+            "manufacture": splitline[2],
+            "price": splitline[3],
+            "size": splitline[4]
+        }
+
+        items.append(
+            item
+        )
+
+
+def stat():
+    manufacturesStats = []
+    sizeStats = []
+
+    statsItem={
+        "name": name,
+        "count": count
+    }
+
+    for item in items:
+        manufacturesStats[item["name"]] = manufacturesStats[item["name"]] + 1
+        sizeStats[item["size"]] = sizeStats[item["size"]] + 1
+    print(manufacturesStats)
+    print(sizeStats)
+
+
+def delete(str):
+    for item in items:
+        if item["name"] == str:
+            items.remove(item)
+
 
 while True:
     command = int(input("> "))
@@ -14,13 +59,26 @@ while True:
         break
     elif command == 2:
         print(items)
+    elif command == 4:
+        readfile()
+    elif command == 5:
+        stat()
+    elif command == 6:
+        name = str(input("Название: "))
+        delete(name)
     elif command == 1:
         name = str(input("Название: "))
         count = int(input("Количество: "))
+        manufacture = str(input("Производитель: "))
+        price = int(input("Цена: "))
+        size = int(input("Размер: "))
 
         item = {
             "name": name,
-            "count": count
+            "count": count,
+            "manufacture": manufacture,
+            "price": price,
+            "size": size
         }
 
         items.append(item)
